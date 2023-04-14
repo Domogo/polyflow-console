1b65822f09878b7db5c980713ad3de2d577d777a
TuboFolkBlackMetal <mislav@polyflow.dev>
2023-04-14 14:42:13 +0200

    update

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -2,10 +2,10 @@ import { environment } from "src/environments/environment"
 
 export const BASE_URL = environment.production ? 
     // 'https://backend-prod.polyflow.dev/api/v1' :
-    'https://backend-staging.polyflow.dev/api/graphql':
+    'https://backend-staging.polyflow.dev/api/v1' :
     'https://backend-staging.polyflow.dev/api/v1'
 
 export const BASE_GQL_URL = environment.production ? 
     // 'https://backend-prod.polyflow.dev/api/graphql' :
-    'https://backend-staging.polyflow.dev/api/graphql':
+    'https://backend-staging.polyflow.dev/api/graphql' :
     'https://backend-staging.polyflow.dev/api/graphql'
