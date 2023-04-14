29c3c9d763030562cddc5c31626af2609652528d
TuboFolkBlackMetal <mislav@polyflow.dev>
2023-04-14 14:22:10 +0200

    fixed urls to staging only

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,9 +1,11 @@
 import { environment } from "src/environments/environment"
 
 export const BASE_URL = environment.production ? 
-    'https://backend-prod.polyflow.dev/api/v1' :
+    // 'https://backend-prod.polyflow.dev/api/v1' :
+    'https://backend-staging.polyflow.dev/api/graphql':
     'https://backend-staging.polyflow.dev/api/v1'
 
 export const BASE_GQL_URL = environment.production ? 
-    'https://backend-prod.polyflow.dev/api/graphql' :
+    // 'https://backend-prod.polyflow.dev/api/graphql' :
+    'https://backend-staging.polyflow.dev/api/graphql':
     'https://backend-staging.polyflow.dev/api/graphql'
