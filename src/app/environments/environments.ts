138d18f35238770f06f48bb54664b90a3349abf7
TuboFolkBlackMetal <mislav@polyflow.dev>
2023-05-05 12:14:11 +0200

    update staging params

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,9 +1,9 @@
 import { environment } from "src/environments/environment"
 
 export const BASE_URL = environment.production ? 
-    'https://backend-prod.polyflow.dev/api/v1' :
+    'https://backend-staging.polyflow.dev/api/v1' :
     'https://backend-staging.polyflow.dev/api/v1'
 
 export const BASE_GQL_URL = environment.production ? 
-    'https://backend-prod.polyflow.dev/api/graphql' :
+    'https://backend-staging.polyflow.dev/api/graphql' :
     'https://backend-staging.polyflow.dev/api/graphql'
